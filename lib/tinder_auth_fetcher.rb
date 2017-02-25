6188dc9ea7e4dc19d4d80794b365553849418772
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2017-02-25 13:47:20 +0900

    finished gemspec and gem itself except spec files

--- lib/tinder_auth_fetcher.rb
+++ lib/tinder_auth_fetcher.rb
@@ -1,5 +1,48 @@
-require "tinder_auth_fetcher/version"
+require 'tinder_auth_fetcher/version'
+require 'mechanize'
 
 module TinderAuthFetcher
-  # Your code goes here...
+  USER_AGENT = 'Mozilla/5.0 (Linux; U; en-gb; KFTHWI Build/JDQ39) AppleWebKit/535.19 (KHTML, like Gecko) Silk/3.16 Safari/535.19'
+  URI = 'https://www.facebook.com/v2.6/dialog/oauth?redirect_uri=fb464891386855067%3A%2F%2Fauthorize%2F&display=touch&state=%7B%22challenge%22%3A%22IUUkEUqIGud332lfu%252BMJhxL4Wlc%253D%22%2C%220_auth_logger_id%22%3A%2230F06532-A1B9-4B10-BB28-B29956C71AB1%22%2C%22com.facebook.sdk_client_state%22%3Atrue%2C%223_method%22%3A%22sfvc_auth%22%7D&scope=user_birthday%2Cuser_photos%2Cuser_education_history%2Cemail%2Cuser_relationship_details%2Cuser_friends%2Cuser_work_history%2Cuser_likes&response_type=token%2Csigned_request&default_audience=friends&return_scopes=true&auth_type=rerequest&client_id=464891386855067&ret=login&sdk=ios&logger_id=30F06532-A1B9-4B10-BB28-B29956C71AB1&ext=1470840777&hash=AeZqkIcf-NEW6vBd'
+
+  def self.fetch_token(email, password)
+    # Login to the page
+    logged_in = login(email, password)
+
+    # Click OK
+    confirmed = confirm(logged_in)
+
+    # Retrieve token
+    confirmed.content.match(/access_token=(\w+)&expires_in=/)[1]
+  end
+
+  private
+
+  def prepare_agent
+    agent = Mechanize.new
+    agent.user_agent = USER_AGENT
+    agent
+  end
+
+  def login(email, password)
+    agent = prepare_agent
+    page = agent.get(URI)
+
+    f = page.forms[0]
+    f.field_with(name: 'email').value = email
+    f.field_with(name: 'pass').value = password
+
+    res = agent.submit f
+
+    if res.uri.path == '/login/'
+      raise 'Facebook login failed. Check if you passed correct email and password'
+    end
+
+    res
+  end
+
+  def confirm(logged_in)
+    confirm = logged_in.forms[0]
+    confirm.click_button(confirm.button_with(name: '__CONFIRM__'))
+  end
 end
--- tinder_auth_fetcher.gemspec
+++ tinder_auth_fetcher.gemspec
@@ -7,11 +7,10 @@ Gem::Specification.new do |spec|
   spec.name          = "tinder_auth_fetcher"
   spec.version       = TinderAuthFetcher::VERSION
   spec.authors       = ["Shuhei Kitagawa"]
-  spec.email         = ["shuhei.kitagawa@c-fo.com"]
+  spec.email         = ["shuhei.kitagawa@gmail.com"]
 
-  spec.summary       = %q{TODO: Write a short summary, because Rubygems requires one.}
-  spec.description   = %q{TODO: Write a longer description or delete this line.}
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
+  spec.summary       = %q{TinderAuthFetcher enables you to retrieve a Facebook authentication token, which is necessary to use Tinder API}
+  spec.homepage      = "https://github.com/shuheiktgw/tinder_auth_fetcher"
   spec.license       = "MIT"
 
   # Prevent pushing this gem to RubyGems.org. To allow pushes either set the 'allowed_push_host'
@@ -30,6 +29,7 @@ Gem::Specification.new do |spec|
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
 
+  spec.add_dependency "mechanize", "~> 2.0"
   spec.add_development_dependency "bundler", "~> 1.14"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec", "~> 3.0"
