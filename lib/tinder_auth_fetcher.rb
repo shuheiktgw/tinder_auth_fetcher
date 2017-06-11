94b386296f0b51f5bc25562d4267c483114637ae
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2017-06-11 13:37:07 +0900

    add a normal pattern test cese

--- lib/tinder_auth_fetcher.rb
+++ lib/tinder_auth_fetcher.rb
@@ -5,6 +5,8 @@ module TinderAuthFetcher
   USER_AGENT = 'Mozilla/5.0 (Linux; U; en-gb; KFTHWI Build/JDQ39) AppleWebKit/535.19 (KHTML, like Gecko) Silk/3.16 Safari/535.19'
   URI = 'https://www.facebook.com/v2.6/dialog/oauth?redirect_uri=fb464891386855067%3A%2F%2Fauthorize%2F&display=touch&state=%7B%22challenge%22%3A%22IUUkEUqIGud332lfu%252BMJhxL4Wlc%253D%22%2C%220_auth_logger_id%22%3A%2230F06532-A1B9-4B10-BB28-B29956C71AB1%22%2C%22com.facebook.sdk_client_state%22%3Atrue%2C%223_method%22%3A%22sfvc_auth%22%7D&scope=user_birthday%2Cuser_photos%2Cuser_education_history%2Cemail%2Cuser_relationship_details%2Cuser_friends%2Cuser_work_history%2Cuser_likes&response_type=token%2Csigned_request&default_audience=friends&return_scopes=true&auth_type=rerequest&client_id=464891386855067&ret=login&sdk=ios&logger_id=30F06532-A1B9-4B10-BB28-B29956C71AB1&ext=1470840777&hash=AeZqkIcf-NEW6vBd'
 
+  class FacebookAuthenticationError < StandardError; end
+
   def self.fetch_token(email, password)
     # Login to the page
     logged_in = login(email, password)
@@ -33,7 +35,7 @@ module TinderAuthFetcher
     res = agent.submit f
 
     if res.uri.path.start_with? '/login'
-      raise FacebookAuthenticationError, 'Facebook Authentication failed. Check if you passed correct email and password'
+      raise TinderAuthFetcher::FacebookAuthenticationError, 'Facebook Authentication failed. Check if you passed correct email and password'
     end
 
     res
@@ -46,5 +48,3 @@ module TinderAuthFetcher
 
   private_class_method :prepare_agent, :login, :confirm
 end
-
-class FacebookAuthenticationError < StandardError; end
--- spec/tinder_auth_fetcher_spec.rb
+++ spec/tinder_auth_fetcher_spec.rb
@@ -7,7 +7,22 @@ RSpec.describe TinderAuthFetcher do
 
   describe '#self.fetch_token' do
     it 'should raise FacebookAuthenticationError if wrong email or password given' do
-      expect { TinderAuthFetcher.fetch_token('wrong email', 'wrong_password') }.to raise_error(FacebookAuthenticationError, 'Facebook Authentication failed. Check if you passed correct email and password')
+      expect { TinderAuthFetcher.fetch_token('wrong email', 'wrong_password') }.to raise_error(TinderAuthFetcher::FacebookAuthenticationError, 'Facebook Authentication failed. Check if you passed correct email and password')
+    end
+
+    # You have to set environment variable TINDER_AUTH_EMAIL, TINDER_AUTH_PASSWORD with your valid Facebook email address and password
+    #
+    # ex:
+    #
+    # ~/.bashrc
+    # export TINDER_AUTH_EMAIL="you_facebook_email@gmail.com"
+    # export TINDER_AUTH_PASSWORD="you_facebook_password"
+
+    it 'should fetch a correct token' do
+      email = ENV['TINDER_AUTH_EMAIL'] || raise('You have to set an environment variable with TINDER_AUTH_EMAIL before execute this test')
+      password = ENV['TINDER_AUTH_PASSWORD'] || raise('You have to set an environment variable with TINDER_AUTH_PASSWORD before execute this test')
+
+      expect(TinderAuthFetcher.fetch_token(email, password).length).not_to eq(0)
     end
   end
 end
